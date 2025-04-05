f3def17c5e773a1c9970781127cbef6441a53964
sbremer05 <utw5es@virginia.edu>
2025-04-05 01:23:14 -0400

    Initial Commit

---
+++ PlanIt/PlanItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanItApp.swift
+//  PlanIt
+//
+//  Created by Sean Bremer on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PlanItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanItTests/PlanItTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanItTests.swift
+//  PlanItTests
+//
+//  Created by Sean Bremer on 4/5/25.
+//
+
+import Testing
+@testable import PlanIt
+
+struct PlanItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
