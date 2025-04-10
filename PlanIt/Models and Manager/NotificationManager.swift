7cc820353fc59fc79d8255fde209199f081f551d
sbremer05 <utw5es@virginia.edu>
2025-04-10 14:37:15 -0400

    Notification requests added. notification mnager file added (empty)

---
+++ PlanIt/Models and Manager/NotificationManager.swift
@@ -0,0 +1,7 @@
+//
+//  NotificationManager.swift
+//  PlanIt
+//
+//  Created by Sean Bremer on 4/10/25.
+//
+
--- PlanIt/PlanItApp.swift
+++ PlanIt/PlanItApp.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 @main
 struct PlanItApp: App {
+    init() {
+        requestNotificationPermission()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         .modelContainer(for: [Event.self])
+    }
+    
+    private func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print("Error requesting notification permission: \(error)")
+            }
+        }
     }
 }
